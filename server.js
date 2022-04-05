cf0bb90c738a0c02c1d75d000ecc9e90e0e41f8f
yu <mark2020.yu@gmail.com>
2022-04-05 23:59:11 +0800

    3. add function POST/DELETE-1/PATCH

--- server.js
+++ server.js
@@ -20,14 +20,53 @@ const requestListener = (req, res) => {
     if(req.url == "/todos" && req.method == "GET"){
         resWriteData(res,todos);
     }else if(req.url == "/todos" && req.method == "POST"){
-
+        req.on("end", () => {
+            try {
+                const title = JSON.parse(body).title;
+                if (title !== undefined) {
+                    const todo = {
+                        title: title,
+                        id: uuidv4(),
+                    };
+                    todo.push(todo);
+                    resWriteData(res,todo);
+                } else {
+                    errorHandle(res,40001);
+                    return false;
+                }
+            } catch (error) {
+                errorHandle(res,40002);
+            }
+        })
     }else if(req.url == "/todos" && req.method == "DELETE"){
         todos.length = 0;
         resWriteData(res,todos);
     }else if(req.url.startsWith("/todos/") && req.method == "DELETE"){
-
+        const id = res.url.split("/").pop();
+        const index = todos.findIndex(element => element.id == id);
+        const todo = {...todos[index]};
+        if (index !== -1) {
+            todos.splice(index, 1);
+            resWriteData(res,todo);
+        } else {
+            errorHandle(res,40001);
+        }
     }else if(req.url.startsWith("/todos/") && req.method == "PATCH"){
-
+        req.on("end", () => {
+            try {
+                const id = res.url.split("/").pop();
+                const index = todos.findIndex(element => element.id == id);
+                const todo = JSON.parse(body).title;
+                if (index !== -1 && todo !== undefined) {
+                    todos[index].title = todo;
+                    resWriteData(res,todos[index]);
+                } else {
+                    errorHandle(res,40001);
+                }
+            } catch (error) {
+                errorHandle(res,40002);
+            }
+        })
     }else if( req.method == "OPTIONS"){
         res.writeHead(200, Headers);
         res.end();
