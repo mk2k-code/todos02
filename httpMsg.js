7aedae1ec67c03e71b97493c531c35ae7026e399
yu <mark2020.yu@gmail.com>
2022-04-05 23:32:04 +0800

    1. add httpMsg.js server.js(template)

--- httpMsg.js
+++ httpMsg.js
@@ -1,1 +1,49 @@
-y
+const headers = {
+	"Access-Control-Allow-Headers": "Content-Type, Authorization, Content-Length, X-Requested-With",
+	"Access-Control-Allow-Origin": "*",
+	"Access-Control-Allow-Methods": "PATCH, POST, GET, OPTIONS, DELETE",
+	"Content-Type": "application/json",
+};
+
+function errorHandle(res, errorCode) {
+	let messgae = "";
+	if (errorCode == 404) {
+		res.writeHead(404, headers);
+		res.write(
+			JSON.stringify({
+				statsu: "false",
+				data: "無此路由!",
+			})
+		);
+		res.end();
+		return false;
+	}
+	if (errorCode == 40001) {
+		message = "無此資料欄位或資料欄位未填寫正確!";
+	} else if (errorCode == 40002) {
+		message = "JSON格式錯誤,請確認資料格式!";
+	} else {
+		message = "無此錯誤訊息!";
+	}
+	res.writeHead(400, headers);
+	res.write(
+		JSON.stringify({
+			status: "false",
+			message: message,
+		})
+	);
+	res.end();
+}
+
+function resWiteData(res, data) {
+	res.writeHead(200, headers);
+	res.write(
+		JSON.stringify({
+			status: "sucess",
+			data: data,
+		})
+	);
+	res.end();
+}
+
+module.exports = { errorHandle, resWiteData };
---
+++ server.js
@@ -0,0 +1,39 @@
+const http = require('http');
+const  { v4: uuidv4 } = require('uuid');
+const  {errorHandle, resWriteData} = require('./httpMsg');
+const port = process.env.PORT || 3005;
+
+todos = []
+const requestListener = (req, res) => {
+    const Headers = {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
+        "Access-Control-Allow-Methods": "GET, POST, PATCH, DELETE, OPTIONS",
+        "Content-Type": "application/json",
+    };
+
+    let body = "";
+    req.on("data", (chunk) => {
+        body += chunk;
+    });
+
+    if(req.url == "/todos" && req.method == "GET"){
+
+    }else if(req.url == "/todos" && req.method == "POST"){
+
+    }else if(req.url == "/todos" && req.method == "DELETE"){
+
+    }else if(req.url.startsWith("/todos/") && req.method == "DELETE"){
+
+    }else if(req.url.startsWith("/todos/") && req.method == "PATCH"){
+
+    }else if( req.method == "OPTIONS"){
+
+    }else{
+
+    }
+};
+const server =http.createServer(requestListener);
+server.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}/todos`);
+});
