fe80a630636405264605d8a96aa24da1eeb01bbc
yu <mark2020.yu@gmail.com>
2022-04-10 23:54:54 +0800

    fix bug: Headers -> headers

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const http = require('http');
-const  { v4: uuidv4 } = require('uuid');
-const  {errorHandle, resWriteData} = require('./httpMsg');
+const http = require("http");
+const { v4: uuidv4 } = require("uuid");
+const { errorHandle, resWriteData } = require("./httpMsg");
 const port = process.env.PORT || 3005;
 
 todos = [];
@@ -12,69 +12,69 @@ const requestListener = (req, res) => {
 		"Content-Type": "application/json",
 	};
 
-    let body = "";
-    req.on("data", (chunk) => {
-        body += chunk;
-    });
+	let body = "";
+	req.on("data", (chunk) => {
+		body += chunk;
+	});
 
-    if(req.url == "/todos" && req.method == "GET"){
-        resWriteData(res,todos);
-    }else if(req.url == "/todos" && req.method == "POST"){
-        req.on("end", () => {
-            try {
-                const title = JSON.parse(body).title;
-                if (title !== undefined) {
-                    const todo = {
-                        title: title,
-                        id: uuidv4(),
-                    };
-                    todos.push(todo);
-                    resWriteData(res,todo);
-                } else {
-                    errorHandle(res,40001);
-                    return false;
-                }
-            } catch (error) {
-                errorHandle(res,40002);
-            }
-        })
-    }else if(req.url == "/todos" && req.method == "DELETE"){
-        todos.length = 0;
-        resWriteData(res,todos);
-    }else if(req.url.startsWith("/todos/") && req.method == "DELETE") {
-        const id = req.url.split("/").pop();
-        const index = todos.findIndex(element => element.id == id);
-        const todo = {...todos[index]};
-        if (index !== -1) {
-            todos.splice(index, 1);
-            resWriteData(res,todo);
-        } else {
-            errorHandle(res,40001);
-        }
-    }else if(req.url.startsWith("/todos/") && req.method == "PATCH"){
-        req.on("end", () => {
-            try {
-                const id = req.url.split("/").pop();
-                const index = todos.findIndex(element => element.id == id);
-                const todo = JSON.parse(body).title;
-                if (index !== -1 && todo !== undefined) {
-                    todos[index].title = todo;
-                    resWriteData(res,todos[index]);
-                } else {
-                    errorHandle(res,40001);
-                }
-            } catch (error) {
-                errorHandle(res, 40002);
-            }
-        });
-    }else if( req.method == "OPTIONS"){
-        res.writeHead(200, Headers);
-        res.end();
-    }else{
-        errorHandle(res,404);
-    }
+	if (req.url == "/todos" && req.method == "GET") {
+		resWriteData(res, todos);
+	} else if (req.url == "/todos" && req.method == "POST") {
+		req.on("end", () => {
+			try {
+				const title = JSON.parse(body).title;
+				if (title !== undefined) {
+					const todo = {
+						title: title,
+						id: uuidv4(),
+					};
+					todos.push(todo);
+					resWriteData(res, todo);
+				} else {
+					errorHandle(res, 40001);
+					return false;
+				}
+			} catch (error) {
+				errorHandle(res, 40002);
+			}
+		});
+	} else if (req.url == "/todos" && req.method == "DELETE") {
+		todos.length = 0;
+		resWriteData(res, todos);
+	} else if (req.url.startsWith("/todos/") && req.method == "DELETE") {
+		const id = req.url.split("/").pop();
+		const index = todos.findIndex((element) => element.id == id);
+		const todo = { ...todos[index] };
+		if (index !== -1) {
+			todos.splice(index, 1);
+			resWriteData(res, todo);
+		} else {
+			errorHandle(res, 40001);
+		}
+	} else if (req.url.startsWith("/todos/") && req.method == "PATCH") {
+		req.on("end", () => {
+			try {
+				const id = req.url.split("/").pop();
+				const index = todos.findIndex((element) => element.id == id);
+				const todo = JSON.parse(body).title;
+				if (index !== -1 && todo !== undefined) {
+					todos[index].title = todo;
+					resWriteData(res, todos[index]);
+				} else {
+					errorHandle(res, 40001);
+				}
+			} catch (error) {
+				errorHandle(res, 40002);
+			}
+		});
+	} else if (req.method == "OPTIONS") {
+		res.writeHead(200, headers);
+		res.end();
+	} else {
+		errorHandle(res, 404);
+	}
 };
-const server =http.createServer(requestListener);
+const server = http.createServer(requestListener);
 server.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}/todos`);
+	console.log(`Server is running on http://localhost:${port}/todos`);
 });
