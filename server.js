b982a5014395a5088625588c10a53aa0717fe8a4
yu <mark2020.yu@gmail.com>
2022-04-11 00:24:19 +0800

    separate headers to httpHeader file

--- server.js
+++ server.js
@@ -1,17 +1,11 @@
 const http = require("http");
 const { v4: uuidv4 } = require("uuid");
+const { headers } = require("./httpHeader");
 const { errorHandle, resWriteData } = require("./httpMsg");
 const port = process.env.PORT || 3005;
 
 todos = [];
 const requestListener = (req, res) => {
-	const headers = {
-		"Access-Control-Allow-Headers": "Content-Type, Authorization, Content-Length, X-Requested-With",
-		"Access-Control-Allow-Origin": "*",
-		"Access-Control-Allow-Methods": "PATCH, POST, GET, OPTIONS, DELETE",
-		"Content-Type": "application/json",
-	};
-
 	let body = "";
 	req.on("data", (chunk) => {
 		body += chunk;
